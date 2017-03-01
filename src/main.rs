7a059559c4d12a297ffd6baea79b4ce173bd1b03
Carl Colglazier <carl@carlcolglazier.com>
2017-02-28 19:24:28 -0500

    Improve syncing / threading

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,65 @@
 extern crate curl;
 extern crate rustc_serialize;
-extern crate filetime;
-extern crate chrono;
 extern crate dotenv;
 #[macro_use] extern crate serde_derive;
 extern crate serde_json;
 #[macro_use] extern crate diesel;
 #[macro_use] extern crate diesel_codegen;
+extern crate csv;
 
 mod tba;
 mod schema;
 mod models;
 
-use std::fs;
 use diesel::sqlite::SqliteConnection;
 use dotenv::dotenv;
 use diesel::prelude::*;
-use std::env;
-use std::str;
 use models::*;
-use std::thread;
-use std::time;
-use std::io::BufReader;
-use std::io;
-use std::io::prelude::*;
-use std::fs::File;
+use std::{thread, str, env, time};
+use std::fs::OpenOptions;
 use std::error::Error;
+use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
 
 const CURRENT_YEAR: i32 = 2017;
 
-pub fn db_connect() -> SqliteConnection {
+struct RequestData {
+    events: Vec<models::Event>,
+    matches: Vec<models::GameMatch>,
+}
+
+#[derive(RustcDecodable)]
+struct HistoryRecord {
+    url: String,
+    time: String,
+}
+
+fn open_history() -> HashMap<String, String> {
+    let f = OpenOptions::new().create(true).write(true).open("tba_history.csv").unwrap();
+    drop(f);
+    let mut map: HashMap<String, String> = HashMap::new();
+    let mut rdr = csv::Reader::from_file("tba_history.csv").unwrap();
+    for record in rdr.decode() {
+        let record: HistoryRecord = match record {
+            Ok(r) => r,
+            Err(e) => {
+                panic!("ERROR: {}", e.description());
+                //continue;
+            },
+        };
+        map.insert(record.url, record.time);
+    }
+    return map;
+}
+
+fn write_history(map: &HashMap<String, String>) {
+    let mut wtr = csv::Writer::from_file("tba_history.csv").unwrap();
+    for record in map.iter() {
+        let _ = wtr.encode(record);
+    }
+}
+
+fn db_connect() -> SqliteConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").
@@ -40,17 +70,79 @@ pub fn db_connect() -> SqliteConnection {
 
 fn setup() {
     let mut threads = Vec::new();
+    let history = Arc::new(Mutex::new(open_history()));
+    let request_data: Arc<Mutex<RequestData>> =
+        Arc::new(Mutex::new(RequestData {
+            events: Vec::new(),
+            matches: Vec::new(),
+        }));
     for i in 2002..CURRENT_YEAR {
+        let request_data = request_data.clone();
+        let history = history.clone();
         threads.push(thread::spawn(move || {
+            let mut info = RequestData {
+                events: Vec::new(),
+                matches: Vec::new(),
+            };
             let url = format!("events/{}", i);
-            let data = tba::request(&url);
+            let mut last_time = String::new();
+            {
+                let history = history.lock()
+                    .expect("Could not lock history for getting event time");
+                match history.get(&url) {
+                    Some(date) => last_time.push_str(&date),
+                    None => {},
+                };
+            }
+            let response = tba::request(&url, &last_time);
+            if response.code != 200 {
+                return;
+            }
+            let data = response.data;
+            {
+                let mut history = history.lock()
+                    .expect("Could not lock history for setting event time");
+                history.insert(url, response.last_modified.trim().to_string());
+            }
             if data.len() > 0 {
-                let data_str = str::from_utf8(&data).unwrap();
-                let events: Vec<models::Event> = serde_json::from_str(&data_str).unwrap();
-                for event in events {
+                let data_str = str::from_utf8(&data)
+                    .expect("Could not load data string");
+                let mut events: Vec<models::Event> = serde_json::from_str(&data_str)
+                    .expect("Could not parse events JSON");
+                info.events.append(&mut events);
+                for event in &info.events {
                     let url = format!("event/{}/matches", event.key);
-                    let data = tba::request(&url);
+                    let mut last_time = String::new();
+                    {
+                        let history = history.lock()
+                            .expect("Could not get history for match reading");
+                        match history.get(&url) {
+                            Some(date) => last_time.push_str(&date),
+                            None => {},
+                        };
+                    }
+                    let response = tba::request(&url, &last_time);
+                    if response.code != 200 {
+                        continue;
+                    }
+                    let data = response.data;
+                    {
+                        let mut history = history.lock()
+                            .expect("Could not get history for match writing");
+                        history.insert(url, response.last_modified.trim().to_string());
+                    }
+                    let data_str = str::from_utf8(&data)
+                        .expect("Could not load match data string");
+                    let mut matches: Vec<models::GameMatch> = match serde_json::from_str(&data_str) {
+                        Ok(m) => m,
+                        Err(_) => continue,
+                    };
+                    info.matches.append(&mut matches);
                 }
+                let mut request_data = request_data.lock()
+                    .expect("Could not lock request data");
+                request_data.events.append(&mut info.events);
+                request_data.matches.append(&mut info.matches);
             }
         }));
     }
@@ -58,49 +150,31 @@ fn setup() {
         let _ = child.join();
     }
     let conn = db_connect();
-    let temp = File::open("temp.txt").expect("Could not open temp.txt");
-    let mut reader = BufReader::new(temp);
-    let mut line = String::new();
-    for l in reader.lines() {
-        let line = l.unwrap();
-        if line.contains("events") {
-            let mut event_file = match File::open(&line) {
-                Ok(file) => file,
-                Err(e) => panic!("Could not find \"{}\": {}", line, e.description()),
-            };
-            let mut file_buffer = String::new();
-            event_file.read_to_string(&mut file_buffer);
-            let events: Vec<models::Event> = serde_json::from_str(&file_buffer).unwrap();
-            for event in events {
-                match create_event(&conn, &event) {
-                    Ok(_) => {},
-                    Err(e) => {
-                        println!("Error: {}", e.description());
-                        break;
-                    },
-                };
-            }
-        } else if line.contains("event") {
-            let mut match_file = File::open(&line).expect(&format!("Could not find {}", line));
-            let mut file_buffer = String::new();
-            match_file.read_to_string(&mut file_buffer);
-            let matches: Vec<models::GameMatch> = serde_json::from_str(&file_buffer).unwrap();
-            for m in matches {
-                match create_match(&conn, &m) {
-                    Ok(_) => {},
-                    Err(e) => {
-                        println!("Error: {}", e.description());
-                        thread::sleep(time::Duration::from_millis(50));
-                    },
-                };
-            }
-        }
+    let result = request_data.lock().unwrap();
+    for event in &result.events {
+        match create_event(&conn, &event) {
+            Ok(_) => {},
+            Err(e) => {
+                println!("Error: {}", e.description());
+                break;
+            },
+        };
     }
-    fs::remove_file("temp.txt");
+    for m in &result.matches {
+        match create_match(&conn, &m) {
+            Ok(_) => {},
+            Err(e) => {
+                println!("Error: {}", e.description());
+                thread::sleep(time::Duration::from_millis(50));
+            },
+        };
+    }
+    let history = history.lock().unwrap();
+    write_history(&history);
 }
 
 
 fn main() {
-    fs::create_dir_all(tba::TBA_DATA_DIR).unwrap();
+    //fs::create_dir_all(tba::TBA_DATA_DIR).unwrap();
     setup();
 }
--- src/tba.rs
+++ src/tba.rs
@@ -1,70 +1,50 @@
-use std::fs;
 use curl::easy::{Easy, List};
-use filetime::FileTime;
-use chrono::prelude::*;
-use chrono::naive::datetime::NaiveDateTime;
-use chrono::DateTime;
-use std::io::prelude::*;
-use std::fs::File;
-use std::path::Path;
-use std::fs::OpenOptions;
+use std::str;
 
-pub const TBA_DATA_DIR: &'static str = "./tba_data";
-
-/// Find the last time that the request has been made based on the cache files.
-/// Returns 0 if no such file exists.
-fn last_checked(url_ext: &str) -> u64 {
-    let local_file_path_str = format!("{}/{}", TBA_DATA_DIR, url_ext);
-    let metadata = match fs::metadata(local_file_path_str) {
-        Ok(meta) => meta,
-        Err(_) => return 0,
-    };
-    let modified_time = FileTime::from_last_modification_time(&metadata);
-    // NOTE: This is Linux specific. Time will be different on Windows!
-    return modified_time.seconds();
+pub struct Response {
+    pub code: u32,
+    pub data: Vec<u8>,
+    pub last_modified: String,
 }
 
-fn write_file(url_ext: &str, output: &[u8]) {
-    let path_str = format!("{}/{}", TBA_DATA_DIR, url_ext);
-    let path = Path::new(&path_str);
-    if !path.exists() {
-        fs::create_dir_all(path.parent().unwrap()).unwrap();
-    }
-    let buffer = File::create(&path_str);
-    buffer.unwrap().write(output);
-    OpenOptions::new().write(true).append(true).create(true)
-        .open("temp.txt").unwrap().write(format!("{}\n", path_str).as_bytes());
-}
-
-pub fn request(url_ext: &str) -> Vec<u8> {
+pub fn request(url_ext: &str, date: &str) -> Response {
     let request_url = format!("https://www.thebluealliance.com/api/v2/{}", url_ext);
-    let last_time = last_checked(url_ext);
-    let time = DateTime::<UTC>::from_utc(NaiveDateTime::from_timestamp(last_time as i64, 0), UTC);
+    //let last_time = last_checked(url_ext);
+    //let time = DateTime::<UTC>::from_utc(NaiveDateTime::from_timestamp(last_time as i64, 0), UTC);
     let mut easy = Easy::new();
     let mut list = List::new();
     let mut data = Vec::new();
     list.append("X-TBA-App-Id: Carl Colglazier:FRC ELO:0.0.0").unwrap();
-    if last_time > 0 {
-        let time_header = format!("If-Modified-Since: {}", time.to_rfc2822());
+    if date.len() > 0 {
+        let time_header = format!("If-Modified-Since: {}", date);
         list.append(&time_header).unwrap();
-        println!("{}", time_header);
+        println!("{}, {}", url_ext, time_header);
     }
     easy.http_headers(list).unwrap();
     easy.url(&request_url).unwrap();
+    let mut headers = String::new();
     {
         let mut transfer = easy.transfer();
         transfer.write_function(|new_data| {
             data.extend_from_slice(new_data);
             Ok(new_data.len())
         }).unwrap();
+        transfer.header_function(|header| {
+            let s = str::from_utf8(header).unwrap().to_string();
+            if s.starts_with("Last-Modified: ") {
+                headers.push_str(&s[15..]);
+            }
+            true
+        }).unwrap();
         transfer.perform().unwrap();
     }
     let code = easy.response_code().unwrap();
     if code == 200 {
-        write_file(url_ext, &data);
         println!("Updating {}", url_ext);
-    } else {
-        data.clear();
     }
-    return data;
+    return Response {
+        code: code,
+        data: data,
+        last_modified: headers,
+    };
 }
